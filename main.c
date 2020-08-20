2c860001e92c49b76aecde66bce992bb916466e5
Shoichiro Kawauchi <lacrosse91.ut@gmail.com>
2020-08-20 14:47:02 +0000

    整数を一つ出力するコンパイラを作成した

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
-/*
-42をリターンする関数
-echo $?とターミナルで打つと42が返ってくるようになる。
-exit statusとも言う
-*/
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    return 42;
+/*
+argc -> 引数の数。
+argv -> それぞれの引数が格納された配列。argv[0]は実行するファイル名になるため, argv[1]を読み取る。
+コマンドから数値を読み込んでアセンブリに埋め込むプログラム
+
+cc -o mycc mycc.c
+./mycc 91 > tmp.s
+*/
+int main(int argc, char **argv) {
+
+    if (argc != 2) {
+        fprintf(stderr, "引数の個数が間違っています\n");
+        return 1;
+    }
+
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
+    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  ret\n");
+    return 0;
+
 }
-
-/*
-cc -o main main.c
-./main
-echo $?
-*/
